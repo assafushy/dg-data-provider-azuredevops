9f189312bf54f1f8ce2e8acd44d3e9d5a6c4bbc1
denispankove <92787105+denispankove@users.noreply.github.com>
2022-01-13 19:15:09 +0200

    Features/data provider/get history (#13)
    * added get all commits for repo + test
    
    * added get pipeline and release history + tests
    
    * added get all pipelines,releases + tests
    
    * removed code smells
    
    * removed code smells #2
    
    * adding await to data prividers

--- src/modules/GitDataProvider.ts
+++ src/modules/GitDataProvider.ts
@@ -261,13 +261,14 @@ export default class GitDataProvider {
     repoID: string
   ){ 
     let url: string = `${this.orgUrl}${projectName}/_apis/git/repositories/${repoID}/commits?api-version=${this.apiVersion}`
-    return TFSServices.getItemContent(
+    let res:any = await TFSServices.getItemContent(
       url,
       this.token,
       "get",
       null,
       null
     );
+    return res;
   }
   
 }
